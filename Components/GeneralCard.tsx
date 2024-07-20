e02e4f91556681294720958d21fa37863217f4f5
giga-gogosashvili <gogosashvili@gmail.com>
2024-07-20 15:07:13 +0100

    deploy 5

--- Components/GeneralCard.tsx
+++ Components/GeneralCard.tsx
@@ -4,7 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import mainphoto from 'src/assets/11235885_11070.jpg';
+import mainphoto from '../src/assets/11235885_11070.jpg';
 
 export default function GeneralCard() {
   return (
