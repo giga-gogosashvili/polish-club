2dfdc97b11ac6d5212002320aab4e59dd71e5d72
giga-gogosashvili <gogosashvili@gmail.com>
2024-08-15 14:56:24 +0100

    magda page icons

--- Components/Magda.tsx
+++ Components/Magda.tsx
@@ -8,9 +8,15 @@ import Image from 'mui-image';
 
 import GoBack from './GoBack';
 import BottomAppBar from './BottomAppBar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import Icon from '@mdi/react';
+import { mdiHumanMaleBoard } from '@mdi/js';
+import { mdiCalendarCheck } from '@mdi/js';
+import { mdiAccountTieWoman } from '@mdi/js';
+import { mdiEyeCheck } from '@mdi/js';
+import { mdiMedalOutline } from '@mdi/js';
+import { mdiSchoolOutline } from '@mdi/js';
+
+import Stack from '@mui/material/Stack';
 
 export default function Magda() {
   return (
@@ -47,40 +53,115 @@ export default function Magda() {
                 Magdalena Knapik
               </Typography>
             </Box>
+            <Box marginLeft="20px">
+              <Stack alignItems="center" direction="row" gap={2}>
+                <Icon color="#000" path={mdiHumanMaleBoard} size={1} />{' '}
+                <Typography variant="body1" color="#000">
+                  wykładowca:
+                </Typography>
+              </Stack>
+              <Typography
+                variant="body1"
+                color="#000"
+                align="left"
+                marginLeft="55px"
+                // sx={{ display: 'block' }}
+              >
+                {' '}
+                - języka polskiego jako obcego od 2003 roku
+              </Typography>
+              <Typography
+                variant="body1"
+                color="#000"
+                align="left"
+                marginLeft="55px"
+                // sx={{ display: 'block' }}
+              >
+                {' '}
+                - Wiedzy o Polsce
+              </Typography>
+              <Typography
+                variant="body1"
+                color="#000"
+                align="left"
+                marginLeft="55px"
+                // sx={{ display: 'block' }}
+              >
+                {' '}
+                - z zakresu glottodydaktyki, literatury, teorii komunikacji,
+                kultury, technik multimedialnych, Polski w świecie
+              </Typography>
 
-            <List sx={{ marginLeft: '10%' }}>
-              <ListItem disablePadding>
-                <ListItemText sx={{ alignSelf: 'center' }} disableTypography>
-                  {' '}
-                  <Box color="#000">
-                    - wykładowca języka polskiego jako obcego od 2003 roku text
-                  </Box>
-                  <Box color="#000">- wykładowca Wiedzy o Polsce</Box>
-                  <Box color="#000">
-                    - wykładowca z zakresu glottodydaktyki, literatury, teorii
-                    komunikacji, kultury, technik multimedialnych, Polski w
-                    świecie
-                  </Box>
-                  <Box color="#000">
-                    - organizatorka wydarzeń kulturalnych i językowych
-                  </Box>
-                  <Box color="#000">
-                    - przewodnicząca egzaminów certyfikatowych z języka
-                    polskiego jako obcego
-                  </Box>
-                  <Box color="#000">
-                    - organizatorka i egzaminatorka egzaminów wstępnych
-                  </Box>
-                  <Box color="#000">- egzaminatorka TELC</Box>
-                  <Box color="#000">
-                    - laureatka plebiscytu Absolwent z Pasją, nagród rektora UŚ
-                  </Box>
-                  <Box color="#000">- złota odznaka UŚ</Box>
-                </ListItemText>
-              </ListItem>
-            </List>
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon
+                  color="#000"
+                  path={mdiCalendarCheck}
+                  size={1}
+                  align="left"
+                />
+                <Typography variant="body1" color="#000" align="left">
+                  organizatorka wydarzeń kulturalnych i językowych
+                </Typography>
+              </Stack>
 
-            <Typography color="#000">
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon
+                  color="#000"
+                  path={mdiAccountTieWoman}
+                  size={1}
+                  align="left"
+                />
+                <Typography variant="body1" color="#000" align="left">
+                  przewodnicząca egzaminów certyfikatowych z języka polskiego
+                  jako obcego
+                </Typography>
+              </Stack>
+
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon
+                  color="#000"
+                  path={mdiCalendarCheck}
+                  size={1}
+                  align="left"
+                />
+                <Typography variant="body1" color="#000" align="left">
+                  organizatorka i egzaminatorka egzaminów wstępnych
+                </Typography>
+              </Stack>
+
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon color="#000" path={mdiEyeCheck} size={1} align="left" />
+                <Typography variant="body1" color="#000" align="left">
+                  egzaminatorka TELC
+                </Typography>
+              </Stack>
+
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon
+                  color="#000"
+                  path={mdiSchoolOutline}
+                  size={1}
+                  align="left"
+                />
+                <Typography variant="body1" color="#000" align="left">
+                  laureatka plebiscytu Absolwent z Pasją, nagród rektora
+                  Uniwersytetu Śląskiego
+                </Typography>
+              </Stack>
+
+              <Stack direction="row" gap={2} alignItems="left">
+                <Icon
+                  color="#000"
+                  path={mdiMedalOutline}
+                  size={1}
+                  align="left"
+                />
+                <Typography variant="body1" color="#000" align="left">
+                  złota odznaka UŚ
+                </Typography>
+              </Stack>
+            </Box>
+            <Typography color="#000" marginTop="20px">
               Interesujące informacje o mnie są w{' '}
               <Link href="https://wordwall.net/pl/resource/75401459/polski-dla-cudzoziemc%c3%b3w/magdalena-knapik-poznaj-mnie">
                 quizie
